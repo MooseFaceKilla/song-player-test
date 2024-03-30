51418fff3496be350c1cdbbb51af550f18186aa9
MooseFaceKilla <134459806+MooseFaceKilla@users.noreply.github.com>
2024-03-30 19:51:23 -0400

    Update app.py

--- app.py
+++ app.py
@@ -18,30 +18,31 @@ app = Flask(__name__)
 client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-# Define route for home page
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        artist_name = request.form['artist_name']
-        correct_artist_name, top_tracks = get_top_tracks(artist_name)
-        if top_tracks:
-            return render_template('top_tracks.html', artist=correct_artist_name, tracks=top_tracks)
-        else:
-            error_message = f"No artist found with the name '{artist_name}'."
-            return render_template('index.html', error=error_message)
-    else:
-        return render_template('index.html')
-
-# Function to get artist's top tracks
+# Function to get artist's top tracks and image
 def get_top_tracks(artist_name):
     results = sp.search(q=artist_name, type='artist', limit=1)
     if len(results['artists']['items']) > 0:
         correct_artist_name = results['artists']['items'][0]['name']
         artist_id = results['artists']['items'][0]['id']
         top_tracks = sp.artist_top_tracks(artist_id=artist_id, country='US')['tracks']
-        return correct_artist_name, top_tracks
+        artist_image = results['artists']['items'][0]['images'][0]['url'] if results['artists']['items'][0]['images'] else None
+        return correct_artist_name, top_tracks, artist_image
     else:
-        return None, None
+        return None, None, None
+
+# Define route for home page
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        artist_name = request.form['artist_name']
+        correct_artist_name, top_tracks, artist_image = get_top_tracks(artist_name)
+        if top_tracks:
+            return render_template('top_tracks.html', artist=correct_artist_name, tracks=top_tracks, image_url=artist_image)
+        else:
+            error_message = f"No artist found with the name '{artist_name}'."
+            return render_template('index.html', error=error_message)
+    else:
+        return render_template('index.html')
 
 if __name__ == '__main__':
     serve(app, host='0.0.0.0', port=42069)
