65d3bca411a936cdabd19b21458e6bea5c4e2403
MooseFaceKilla <134459806+MooseFaceKilla@users.noreply.github.com>
2024-04-05 10:03:09 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import requests
 from flask import Flask, request, render_template
 from dotenv import load_dotenv
 import os
@@ -18,31 +19,46 @@ app = Flask(__name__)
 client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-# Function to get artist's top tracks and image
+
+# Function to get artist's top tracks
 def get_top_tracks(artist_name):
     results = sp.search(q=artist_name, type='artist', limit=1)
     if len(results['artists']['items']) > 0:
         correct_artist_name = results['artists']['items'][0]['name']
         artist_id = results['artists']['items'][0]['id']
         top_tracks = sp.artist_top_tracks(artist_id=artist_id, country='US')['tracks']
-        artist_image = results['artists']['items'][0]['images'][0]['url'] if results['artists']['items'][0]['images'] else None
-        return correct_artist_name, top_tracks, artist_image
+        return correct_artist_name, top_tracks
     else:
-        return None, None, None
+        error_message = f"No artist found with the name '{artist_name}'. Please try a different artist."
+        return None, None, error_message
+
 
 # Define route for home page
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        artist_name = request.form['artist_name']
-        correct_artist_name, top_tracks, artist_image = get_top_tracks(artist_name)
-        if top_tracks:
-            return render_template('top_tracks.html', artist=correct_artist_name, tracks=top_tracks, image_url=artist_image)
-        else:
-            error_message = f"No artist found with the name '{artist_name}'."
-            return render_template('index.html', error=error_message)
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/search', methods=['POST'])
+def search():
+    artist_name = request.form['artist_name']
+    correct_artist_name, top_tracks = get_top_tracks(artist_name)
+    if correct_artist_name:
+        return render_template('top_tracks.html', artist=correct_artist_name, tracks=top_tracks)
     else:
-        return render_template('index.html')
+        error_message = f"No artist found with the name '{artist_name}'."
+        return render_template('searchByArtistName.html', error=error_message)
+
+
+@app.route('/hyperlinks')
+def hyperlinks():
+    return render_template('hyperlinks.html')
+
+
+@app.route('/searchByArtistName')
+def searchByArtistName():
+    return render_template('searchByArtistName.html')
+
 
 if __name__ == '__main__':
     serve(app, host='0.0.0.0', port=42069)
